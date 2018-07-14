7610f9a97a621ef88ce321c3922aa73d82577ea2
casey van-roose <kcvanroose@gmail.com>
2018-07-14 18:43:49 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require'pry'
+  def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  data.each do |k, v|
+    v.each do |k2, v2|
+      v2.each do |i|
+        pigeon_list[i] ||= {}
+        pigeon_list[i][k] ||= []
+        pigeon_list[i][k] << k2.to_s
+         binding.pry
+      end
+    end
+  end
+pigeon_list
 end
